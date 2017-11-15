13759076c1d46d51783d17ab2e297e796c395a42
Mayank <mayank.singh081997@gmail.com>
2017-11-15 10:35:14 +0530

    python

---
+++ inputValidation.py
@@ -0,0 +1,16 @@
+while True:
+	age=input("enter your age : ")
+	if age.isdecimal():
+		break
+	else:
+		print("Please Enter Numeric Age ")
+while True:
+	password=input("enter your password : [LETTERS AND NUMBERS ONLY] : ")
+	if password.isalnum():
+		if(password=='googly123'):
+			print('Welcome to the Party !')
+		else:
+			print('Wrong Password !!')
+		break
+	print('Passwords can only have letters and numbers')
+		
