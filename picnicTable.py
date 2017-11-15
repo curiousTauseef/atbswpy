66cb09583da68609964daa2a3c997cedc7731be0
Mayank <mayank.singh081997@gmail.com>
2017-11-15 12:34:45 +0530

    python

---
+++ picnicTable.py
@@ -0,0 +1,16 @@
+#ljust() rjust() center()
+
+def printItems(picnicitems,leftjustify,rightjustify):
+
+	print()
+
+	print('PICNIC ITEMS'.center(leftjustify+rightjustify,'-'))
+
+	for k,v in picnicitems.items():
+
+		print(k.ljust(leftjustify,'.')+str(str(v).rjust(rightjustify)))
+
+picnicItems={'apple':12,'sandwiches':15,'juices':12,'snacks':28}
+
+printItems(picnicItems,19,1)
+
