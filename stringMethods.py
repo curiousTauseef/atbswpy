7535eb120157ae0c51877551fb6ded390834fc4b
Mayank <mayank.singh081997@gmail.com>
2017-11-15 10:09:20 +0530

    python

---
+++ stringMethods.py
@@ -0,0 +1,10 @@
+spam="Hello World!"
+print('spam.upper() : '+spam.upper())
+print('spam.lower() : '+spam.lower())
+print("'H'.isupper() : "+str('H'.isupper()))
+print("'h' isupper() : "+str('h'.isupper()))
+print('spam.lower().islower() : '+ str(spam.lower().islower()))
+print("'123'.isalpha() : "+str('123'.isalpha()))
+print("'123'.isdecimal() : "+str('123'.isdecimal()))
+print("'alpha123'.isalnum() : "+str('alpha123'.isalnum()))
+
