06c671a03841de27354309e17d5e3d6c597d200d
Mayank Singh <mayank.singh081997@gmail.com>
2017-11-22 01:02:22 +0530

    python

--- firstplot.py
+++ firstplot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4,5,6,7,8,9],[1,4,9,16,25,36,49,64,81])
-plt.ylabel('Y values')
-plt.xlabel('X Values')
+plt.plot([2.888,2.988,3.030,3.0692,3.0692,3.1048,3.1377,3.1967,3.2235,3.2487],[-1.30,-0.823,-0.522,-0.301,-0.121,0.021,0.204,0.352,0.477,0.585])
+plt.ylabel('V(output)')
+plt.xlabel('Il')
 plt.show()
